4591d3d73444c33b72f79f7bee322b88ca23ec11
tsukagoshi56 <tsukatoshi0506@gmail.com>
2026-02-03 17:17:27 +0900

    Set default batch size to 8

--- Experiment_RationaleFT/audio_text_mix_e2e_re.py
+++ Experiment_RationaleFT/audio_text_mix_e2e_re.py
@@ -1603,7 +1603,7 @@ def main():
     parser.add_argument("--model_name_or_path", type=str, default="Qwen/Qwen2-Audio-7B-Instruct")
     parser.add_argument("--output_dir", type=str, default="outputs/qwen_rationale_label_ft")
     parser.add_argument("--num_train_epochs", type=int, default=3)
-    parser.add_argument("--batch_size", type=int, default=4)
+    parser.add_argument("--batch_size", type=int, default=8)
     parser.add_argument("--learning_rate", type=float, default=4e-5)
     parser.add_argument("--gradient_accumulation_steps", type=int, default=16)
     parser.add_argument("--max_new_tokens", type=int, default=2048)
